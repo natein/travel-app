6f592ccd710f6975535ea0fd2f8bed2927db9cab
kvadeck <kvadeck@yandex.ru>
2021-03-08 19:51:32 +0300

    feat: sight-gallery component and base markup

---
+++ src/components/Page/SightGallery/constants.jsx
@@ -0,0 +1,43 @@
+export const sightsCollection = {
+  sights: [
+    {
+      _id: "6045cbe836df7e353389d4c4",
+      id: "1a958982-c560-4d64-97fb-6d6d891b6c1a",
+      name: "Berlin Victory Column",
+      description:
+        "The Victory Column (German: About this soundSiegessäule (help·info), from Sieg ‘victory’ + Säule ‘column’) is a monument in Berlin, Germany.",
+      image: "https://source.unsplash.com/t20pc32VbrU/800x600",
+      rating: [],
+    },
+    {
+      _id: "6045cbe836df7e353389d4ce",
+      id: "30a93359-82b5-4276-9a97-285d5a54e335",
+      name: "Reichstag building",
+      description:
+        "The Reichstag (German: Reichstagsgebäude [ˈʁaɪçstaːksɡəˌbɔʏdə]; officially: Deutscher Bundestag – Plenarbereich Reichstagsgebäude [ˈdɔʏtʃɐ ˈbʊndəsˌtaːk ˈpleːnaːɐ̯bəˌʁaɪç ˈʁaɪçstaːksɡəˌbɔʏdə]) is a historic edifice in Berlin, Germany, constructed to house the Imperial Diet (German: Reichstag) of the German Empire.",
+      image: "https://source.unsplash.com/2Bjq3A7rGn4/800x600",
+      rating: [],
+    },
+    {
+      _id: "6045cbe836df7e353389d4d3",
+      id: "56d9d51c-146b-41a6-a2c1-271e90039578",
+      name: "Brandenburg Gate",
+      description:
+        "The Brandenburg Gate (German: Brandenburger Tor; [ˈbʁandn̩bʊɐ̯gɐ ˈtoːɐ̯]) is an 18th-century neoclassical monument in Berlin, built on the orders of Prussian king Frederick William II after the temporary restoration of order during the Batavian Revolution.",
+      image: "https://source.unsplash.com/3Z70SDuYs5g/800x600",
+      rating: [],
+    },
+  ],
+};
+
+export const styles = {
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 200,
+  },
+  content: {
+    height: 180,
+  },
+};
