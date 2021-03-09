a2a1a8ae02ccf8d9b50dbcac1f4fb57f0760f8a0
aliaksei bulhak <aliaksei.bulhak@nike.com>
2021-03-09 13:09:24 +0300

    fix: replace div with Box component from material ui

--- src/components/CountryMap/CountryMap.jsx
+++ src/components/CountryMap/CountryMap.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import Box from '@material-ui/core/Box';
 import mapboxgl from 'mapbox-gl';
 import PropTypes from 'prop-types';
 import { Box } from '@material-ui/core';
