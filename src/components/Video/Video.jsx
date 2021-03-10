486b10d59d1aa339fb49ab93a87697f7402207dd
kvadeck <kvadeck@yandex.ru>
2021-03-10 18:19:54 +0300

    feat: clean up

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import { getVideoId } from '../../helpers';
