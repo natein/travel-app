c2b4e930b8136801a82bce259f5e3bcd1ef70a5d
Natein <natein@gmail.com>
2021-03-12 17:38:49 +0200

    feat: add weather widget to page

--- src/components/Page/PageContainer.jsx
+++ src/components/Page/PageContainer.jsx
@@ -3,16 +3,23 @@ import { connect } from 'react-redux';
 import { useHistory, useParams } from 'react-router';
 import * as countryActions from '../../actions/countryActions';
 import * as currencyActions from '../../actions/currencyActions';
+import * as weatherActions from '../../actions/weatherActions';
 import Page from './Page';
 import LoadingPage from '../LoadingPage';
 
-const PageContainer = ({ loader, locale = 'en', country, onLoadCountry, error, onLoadCurrency, currency }) => {
+const PageContainer = ({ loader, locale = 'en', country, onLoadCountry, error, 
+    onLoadCurrency, currency, weather, onLoadWeather }) => {
     const { isoCode } = useParams();
     const history = useHistory();
 
     useEffect(() => {
         onLoadCountry(isoCode);
         onLoadCurrency();
+        if (country) {
+            const lat = country.capital.coordinates.lat;
+            const lon = country.capital.coordinates.lon;
+            onLoadWeather(lat, lon, locale);
+        }        
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [locale]);
 
@@ -25,7 +32,7 @@ const PageContainer = ({ loader, locale = 'en', country, onLoadCountry, error, o
     return (
         <>
             {loader && <LoadingPage />}
-            {!loader && !!country && <Page country={country} currency={currency} />}
+            {!loader && !!country && <Page country={country} currency={currency} weather={weather} />}
         </>
     );
 };
@@ -36,11 +43,13 @@ const mapStateToProps = (state) => ({
     locale: state.locale,
     error: state.error,
     currency: state.currency,
+    weather: state.weather,
 });
 
 const mapDispatchToProps = (dispatch) => ({
     onLoadCountry: (isoCode) => dispatch(countryActions.loadCountryInfo(isoCode)),
-    onLoadCurrency: () => dispatch(currencyActions.loadCurrency())
+    onLoadCurrency: () => dispatch(currencyActions.loadCurrency()),
+    onLoadWeather: (lat, lon, lang) => dispatch(weatherActions.loadWeather(lat, lon, lang))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PageContainer);
